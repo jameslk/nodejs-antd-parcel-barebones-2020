7b1af672c37782535d0c0eddf944e15fbb4cb25a
James Koshigoe <james@jameskoshigoe.com>
2020-12-10 10:20:09 -0700

    Allow data to be passed from server into HTML (for e.g. user session info)

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,25 @@
 import 'source-map-support/register';
 import express from 'express';
+import path from 'path';
 import bodyParser from 'body-parser';
 import handleProcessEvents from './handle-process-events';
+import * as liquid from 'liquidjs';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('dist/client'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+const liquidEngine = new liquid.Liquid();
+app.engine('html', liquidEngine.express());
+app.set('views', path.resolve(__dirname, '../client'));
+app.set('view engine', 'html');
+app.use(express.static('dist/client', {index: false}));
+
+app.get('/', async (req, res) => {
+    res.render('index', {exampleData: 'Example Data'});
+});
 
 app.get('/healthchk', async (req, res) => {
     res.send({
